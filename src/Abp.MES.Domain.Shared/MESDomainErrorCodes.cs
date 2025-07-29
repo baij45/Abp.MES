0b91c3b0f49bccad3eae1c892b8d75b3e538dbd2
Administrator <Administrator@DESKTOP-019P14C>
2025-07-29 17:27:42 +0800

    3

--- src/Abp.MES.Domain.Shared/MESDomainErrorCodes.cs
+++ src/Abp.MES.Domain.Shared/MESDomainErrorCodes.cs
@@ -3,5 +3,5 @@
 public static class MESDomainErrorCodes
 {
     /* You can add your business exception error codes here, as constants */
-    public const string AuthorAlreadyExists = "BookStore:00001";
+    public const string AuthorAlreadyExists = "MES:00001";
 }
